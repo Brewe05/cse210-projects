5ce56e4219dc9d1dad131da387a496639f680715
Brewe05 <brentwelsford@gmail.com>
2024-11-13 21:38:08 +0200

    Week 2 Participation activity

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello! How are you?");
+        String greeting = Console.ReadLine();
     }
 }
